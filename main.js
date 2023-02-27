7a3991019967ef5cc49189c360707143896a76cc
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-02-27 14:00:09 +0000

    Add JS validation for password length and matching

---
+++ main.js
@@ -0,0 +1,32 @@
+let passwordField = document.getElementById("password")
+let confirmPasswordField = document.getElementById("confirmPass");
+let lengthValidation = document.getElementById("length");
+let matchPasswordMsg = document.getElementById("match");
+let rg2 = /^[A-Za-z]\w{7,15}$/;
+
+confirmPasswordField.addEventListener("input", passValidation);
+passwordField.addEventListener("input",passValidation);
+
+function passValidation()
+{
+
+    if(!passwordField.value.match(rg2))
+    {
+        lengthValidation.innerHTML = "Password must be longer than 8 characters";
+
+    } else
+    {
+        lengthValidation.innerHTML= "";
+    }
+
+    if(passwordField.value !== confirmPasswordField.value)
+    {
+        confirmPasswordField.classList.add("invalid");
+        matchPasswordMsg.innerHTML = "Password do not match";
+    }else
+    {
+        confirmPasswordField.classList.remove("invalid");
+        matchPasswordMsg.innerHTML = "";
+    }
+}
+
