bc7e9e1cdefad6d5cf80837d538cefec6e795357
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-02-28 16:50:50 +0000

    Add autocomplete Finish pwrd validation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let passwordField = document.getElementById("password")
 let confirmPasswordField = document.getElementById("confirmPass");
 let lengthValidation = document.getElementById("length");
 let matchPasswordMsg = document.getElementById("match");
+let submitBtn = document.getElementById("submitButton");
 let rg2 = /^[A-Za-z]\w{7,15}$/;
 
 confirmPasswordField.addEventListener("input", passValidation);
@@ -13,20 +14,24 @@ function passValidation()
     if(!passwordField.value.match(rg2))
     {
         lengthValidation.innerHTML = "Password must be longer than 8 characters";
+        submitBtn.disabled = true;
 
     } else
     {
         lengthValidation.innerHTML= "";
+        submitBtn.disabled = false;
     }
 
     if(passwordField.value !== confirmPasswordField.value)
     {
         confirmPasswordField.classList.add("invalid");
         matchPasswordMsg.innerHTML = "Password do not match";
+        submitBtn.disabled = true;
     }else
     {
         confirmPasswordField.classList.remove("invalid");
         matchPasswordMsg.innerHTML = "";
+        submitBtn.disabled = false;
     }
 }
 
